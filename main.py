7324b9651ff29b1c3fd10778850e165f74723d99
Spandan Maity <spandanmaity58@gmail.com>
2024-09-12 00:06:35 +0530

    login page draft

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
+from datetime import timedelta
 from typing import List
 from typing import Union
-from fastapi import FastAPI, Request, HTTPException, status
+from fastapi import FastAPI, Request, HTTPException, status, Depends
 from pydantic import BaseModel
+
 from YoutubeSearch import YoutubeSearch
 # from DataProcessing.TextSummarization import TextSummarizer
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
+from typing import Annotated
+
+from Security.OAuth2Security import fake_users_db, UserInDB, get_current_active_user, User, Token, \
+    authenticate_user, ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token, get_all_user, add_user
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 origins = [
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "*"
 ]
 
 class Sentence(BaseModel):
@@ -36,12 +44,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
 from SentenceDetectionGeneratorDetector import SentenceTypeDetection
 
 ys = YoutubeSearch()
 
 @app.get("/heathcheck")
-async def root():
+async def root(token: Annotated[str, Depends(oauth2_scheme)]):
     return {"message": "I am Alive!"}
 
 @app.post("/check-sentence/")
@@ -156,3 +166,49 @@ async def validation_exception_handler(request: Request, exc: Exception):
             )
         },
     )
+
+@app.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+) -> Token:
+    try:
+        user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Incorrect username or password",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        access_token = create_access_token(
+            data={"sub": user.username}, expires_delta=access_token_expires
+        )
+        return Token(access_token=access_token, token_type="bearer")
+    except Exception as e:
+        print('Something went wrong while creating token')
+        print(e)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f'Error has occurred -  {e}'
+        )
+
+
+@app.get("/users/me/", response_model=User)
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return current_user
+
+@app.get("/users/all/")
+async def read_users_all():
+    return await get_all_user()
+
+@app.get("/users/me/items/")
+async def read_own_items(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return [{"item_id": "Foo", "owner": current_user.username}]
+
+@app.post("/users/register/")
+async def register_user(user: User):
+    return await add_user(user)
